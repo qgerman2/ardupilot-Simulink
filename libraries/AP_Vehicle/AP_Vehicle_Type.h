e72a4339894c15f96cc0e3c8ef0079d2a32d48a3
qgerman2 <qgerman2@gmail.com>
2024-02-21 17:36:59 -0300

    restore ap_vehicle_type

--- libraries/AP_Vehicle/AP_Vehicle_Type.h
+++ libraries/AP_Vehicle/AP_Vehicle_Type.h
@@ -46,8 +46,6 @@
 #define APM_BUILD_COPTER_OR_HELI (APM_BUILD_TYPE(APM_BUILD_ArduCopter) || APM_BUILD_TYPE(APM_BUILD_Heli))
 
 #else 
-//#define APM_BUILD_TYPE(type) @Invalid_use_of_APM_BUILD_TYPE
-//#define APM_BUILD_COPTER_OR_HELI @Invalid_use_of_APM_BUILD_COPTER_OR_HELI
-#define APM_BUILD_TYPE(type) 0
-#define APM_BUILD_COPTER_OR_HELI 0
+#define APM_BUILD_TYPE(type) @Invalid_use_of_APM_BUILD_TYPE
+#define APM_BUILD_COPTER_OR_HELI @Invalid_use_of_APM_BUILD_COPTER_OR_HELI
 #endif
